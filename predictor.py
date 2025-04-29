bfec6569ec10e83680ea5e829b5369f6d3ad492a
Changfeng-Du <1606332800@qq.com>
2025-04-29 22:28:20 +0800

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -31,15 +31,15 @@ with col1:
                          format_func=lambda x: "Yes" if x == 1 else "No")
     Hypertension = st.selectbox("Hypertension:", options=[1, 2], 
                                 format_func=lambda x: "No" if x == 2 else "Yes")
-    HHR = st.number_input("HHR Ratio:", min_value=0.23, max_value=5.0, value=1.0)
-    NLR = st.number_input("NLR Ratio:", min_value=0.01, max_value=10.0, value=1.0)
+    HHR = st.number_input("HHR Ratio:", min_value=0.23, max_value=5.0, value=1.06)
+    NLR = st.number_input("NLR Ratio:", min_value=0.01, max_value=10.0, value=1.97)
     
 with col2:
-    LMR = st.number_input("LMR Ratio:", min_value=0.01, max_value=20.0, value=3.0)
-    INDFMPIR = st.number_input("Poverty Income Ratio:", min_value=0.1, max_value=5.0, value=2.0)
-    LBXWBCSI = st.number_input("White Blood Cell Count (10^9/L):", min_value=1.0, max_value=200.0, value=6.0)
-    LBXRBCSI = st.number_input("Red Blood Cell Count (10^9/L):", min_value=1.0, max_value=10.0, value=3.0)
-    LBXPLTSI = st.number_input("Platelet Cell Count (10^9/L):", min_value=1.0, max_value=1000.0, value=300.0)
+    LMR = st.number_input("LMR Ratio:", min_value=0.01, max_value=20.0, value=2.8)
+    INDFMPIR = st.number_input("Poverty Income Ratio:", min_value=0.1, max_value=5.0, value=0.9)
+    LBXWBCSI = st.number_input("White Blood Cell Count (10^3/μL):", min_value=1.0, max_value=200.0, value=9.4)
+    LBXRBCSI = st.number_input("Red Blood Cell Count (10^3/μL):", min_value=1.0, max_value=10.0, value=4.06)
+    LBXPLTSI = st.number_input("Platelet Cell Count (10^3/μL):", min_value=1.0, max_value=1000.0, value=253.0)
 
 # Process inputs and make predictions
 feature_values = [smoker, drink,sleep,Hypertension,HHR, NLR,LMR,
