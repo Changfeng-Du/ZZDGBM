cfc3d3877397f49dbd62e69b7f03c78061856de2
Changfeng-Du <1606332800@qq.com>
2025-04-29 23:34:23 +0800

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -31,8 +31,8 @@ with col1:
                          format_func=lambda x: "Yes" if x == 1 else "No")
     Hypertension = st.selectbox("Hypertension:", options=[1, 2], 
                                 format_func=lambda x: "No" if x == 2 else "Yes")
-    HHR = st.number_input("HHR Ratio:", min_value=0.23, max_value=5.0, value=1.06)
-    NLR = st.number_input("NLR Ratio:", min_value=0.01, max_value=10.0, value=1.97)
+    HHR = st.number_input("HHR Ratio:", min_value=0.23, max_value=5.0, value=0.62)
+    NLR = st.number_input("NLR Ratio:", min_value=0.01, max_value=10.0, value=5.22)
     
 with col2:
     LMR = st.number_input("LMR Ratio:", min_value=0.01, max_value=20.0, value=5.8)
